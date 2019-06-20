0f4d9bf1de813897b151b5cd3c45f532764d2da8
JackJarvis12 <42149753+JackJarvis12@users.noreply.github.com>
2019-06-20 18:34:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 	
-server.listen(80, "0.0.0.0");
+server.listen(3000);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html')
